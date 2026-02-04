7f84160f39d4947ae47c85ddcf6a02e50bc51f3d
sfrankli21 <sfrankli21@su.edu>
2026-02-04 16:58:24 -0500

    Stuff

---
+++ UnityProject/Assets/Scripts/DataObjects/ControlData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ControlData", menuName = "Game/Control Data")]
+public class ControlData : ScriptableObject
+{
+    public float lookSpeed = 5.0f;
+}
